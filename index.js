36e2d396b6a4214c2e6967bf8ca6af058f2e9431
DANIEL CHAN <dchan513@msn.com>
2017-09-19 02:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
+
+function nestedTarget() {
+  var div = document.getElementsById('#nested')
+  
+  return div.querySelector('div div div div.target')
+}
